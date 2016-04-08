de373a62a1748584389ec7de41a147805658797f
Your Actual Name <Your Actual Email>
2016-04-08 13:24:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+
+ /*------------------------------
+DOCUMENT READY
+ -------------------------------*/
+
+
+ $(document).ready(function() {
+
+/*------------------------------
+LINK
+ -------------------------------*/
+ 	$('.js-like').on('click', function(event) {
+ 	event.preventDefault();
+
+ 	$(this).text('Liked!')
+ 	.closest('.news-item')
+ 	 .addClass('is-liked');
+ 	});
+
+/*------------------------------
+ADD LINK
+ -------------------------------*/
+
+ 	$('.js-add-link').on('click', function(event){
+ 		event.preventDefault();
+
+ 		$('.js-form').toggleClass('is-visible');
+ 	});
+
+/*------------------------------
+MODAL
+ -------------------------------*/
+$('.js-show-modal').on('click', function(event){
+ 		event.preventDefault();
+
+ 		$('.js-modal').addClass('is-visible');
+ 		$('.js-modal-overlay').addClass('is-visible');
+ 	});
+
+$('.js-modal-overlay').on('click', function(event){
+	$('.js-modal').removeClass('is-visible');
+ 	$('.js-modal-overlay').removeClass('is-visible');
+ 	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
